83bdaf39d36592fba2c8ef1165137e1d12b48574
Nikita Moroz <nikitamoroz118@gmail.com>
2022-06-07 13:08:56 +0200

    Implement Task 4
    * Implement Thread.Join approcah
    
    * Implement Thread.Join approcah
    
    * Finish recursive logic
    
    * Implement semaphore approach
    
    * Code improvements
    
    Co-authored-by: Mikita Maroz <Mikita_Maroz@epam.com>

--- MultiThreading.Task4.Threads.Join/Program.cs
+++ MultiThreading.Task4.Threads.Join/Program.cs
@@ -10,11 +10,22 @@
  */
 
 using System;
+using System.Threading;
 
 namespace MultiThreading.Task4.Threads.Join
 {
+    class ThreadState
+    {
+        public int ThreadNumber { get; set; }
+        public int Value { get; set; }
+    }
+
     class Program
     {
+        const int MaxThreadsCount = 10;
+
+        static SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1);
+
         static void Main(string[] args)
         {
             Console.WriteLine("4.	Write a program which recursively creates 10 threads.");
@@ -26,9 +37,97 @@ namespace MultiThreading.Task4.Threads.Join
 
             Console.WriteLine();
 
-            // feel free to add your code
+            PerformThreadJoinApproach();
+
+            Console.WriteLine();
+
+            PerformThreadPoolSemaphoreApproach();
 
             Console.ReadLine();
+        }
+
+        static void PerformThreadJoinApproach()
+        {
+            var threadState = new ThreadState
+            {
+                ThreadNumber = 1,
+                Value = MaxThreadsCount
+            };
+
+            Console.WriteLine("Recursive threads approach:");
+            var thread = RecursiveThread(threadState);
+            thread.Join();
+        }
+
+        static void PerformThreadPoolSemaphoreApproach()
+        {
+            var threadState = new ThreadState
+            {
+                ThreadNumber = 1,
+                Value = MaxThreadsCount
+            };
+
+            Console.WriteLine("Recursive ThreadPool with Semaphore approach:");
+            RecursiveThreadPool(threadState);
+        }
+
+        static Thread RecursiveThread(object state)
+        {
+            var threadState = state as ThreadState;
+
+            if (threadState is null)
+                throw new ArgumentNullException(nameof(state));
+
+            threadState.ThreadNumber++;
+
+            if (threadState.ThreadNumber <= MaxThreadsCount)
+            {
+                var thread = RecursiveThread(threadState);
+                thread.Join();
+            }
+
+            var newThread = new Thread(DecrementStateValue);
+            newThread.Start(threadState);
+            return newThread;
+        }
+
+        static void RecursiveThreadPool(object state)
+        {
+            var threadState = state as ThreadState;
+
+            if (threadState is null)
+                throw new ArgumentNullException(nameof(state));
+
+            threadState.ThreadNumber++;
+
+            if (threadState.ThreadNumber <= MaxThreadsCount)
+            {
+                RecursiveThreadPool(threadState);
+            }
+
+            ThreadPool.QueueUserWorkItem(DecrementStateValueWithSemaphoreLock, state);
+        }
+
+        static void DecrementStateValue(object state)
+        {
+            var threadState = state as ThreadState;
+
+            if (threadState is null)
+                throw new ArgumentNullException(nameof(state));
+
+            var originalValue = threadState.Value;
+            var decrementedValue = --threadState.Value;
+
+            Console.WriteLine($"Initial value = {originalValue}. New Value = {decrementedValue}");
+        }
+
+        static void DecrementStateValueWithSemaphoreLock(object state)
+        {
+            _semaphoreSlim.Wait();
+
+            DecrementStateValue(state);
+
+            _semaphoreSlim.Release(1);
         }
     }
 }
