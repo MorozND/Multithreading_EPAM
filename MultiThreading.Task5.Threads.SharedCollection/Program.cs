de575ec8aa7fcd988d6c55cd0e7f0668c73710f7
Nikita Moroz <nikitamoroz118@gmail.com>
2022-06-07 13:09:15 +0200

    Implement Task 5
    * Implement task
    
    * Final updates
    
    Co-authored-by: Mikita Maroz <Mikita_Maroz@epam.com>

--- MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,11 +5,23 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
+        static AutoResetEvent _writeEvent = new AutoResetEvent(true);
+        static AutoResetEvent _readEvent = new AutoResetEvent(false);
+
+        const int IterationsCount = 10;
+
+        static readonly CancellationTokenSource _cts = new CancellationTokenSource();
+
+        static readonly List<int> _collection = new List<int>();
+
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -17,9 +29,44 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
             Console.WriteLine();
 
-            // feel free to add your code
+            var readTask = Task.Run(ReadCollection, _cts.Token);
+            var writeTask = Task.Run(WriteCollection, _cts.Token);
+
+            Task.WaitAll(readTask, writeTask);
+
+            _writeEvent.Close();
+            _readEvent.Close();
 
             Console.ReadLine();
+        }
+
+        static void PrintCollection()
+        {
+            Console.WriteLine(string.Concat("[ ", string.Join(", ", _collection), " ]"));
+        }
+
+        static void ReadCollection()
+        {
+            for (int i = 0; i < IterationsCount; i++)
+            {
+                _readEvent.WaitOne();
+
+                PrintCollection();
+
+                _writeEvent.Set();
+            }
+        }
+
+        static void WriteCollection()
+        {
+            for (int i = 0; i < IterationsCount; i++)
+            {
+                _writeEvent.WaitOne();
+
+                _collection.Add(i + 1);
+
+                _readEvent.Set();
+            }
         }
     }
 }
