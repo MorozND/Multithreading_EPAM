daf1454c6ef0a83a7f322596c08a50dcd868d906
Nikita Moroz <nikitamoroz118@gmail.com>
2022-06-07 13:08:18 +0200

    Implement Task 2
    * Add action methods
    
    * Add Output methods
    
    * Finish task
    
    * Code refactoring
    
    * Final Improvements
    
    Co-authored-by: Mikita Maroz <Mikita_Maroz@epam.com>

--- MultiThreading.Task2.Chaining/Program.cs
+++ MultiThreading.Task2.Chaining/Program.cs
@@ -6,12 +6,17 @@
  * Fourth Task – calculates the average value. All this tasks should print the values to console.
  */
 using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task2.Chaining
 {
     class Program
     {
-        static void Main(string[] args)
+        const int RandomAmount = 10;
+
+        static async Task Main(string[] args)
         {
             Console.WriteLine(".Net Mentoring Program. MultiThreading V1 ");
             Console.WriteLine("2.	Write a program, which creates a chain of four Tasks.");
@@ -21,7 +26,15 @@ namespace MultiThreading.Task2.Chaining
             Console.WriteLine("Fourth Task – calculates the average value. All this tasks should print the values to console");
             Console.WriteLine();
 
-            // feel free to add your code
+            var cts = new CancellationTokenSource();
+            var taskActionFactory = new TaskActionFactory(new ConsoleOutputService());
+
+            var taskNumber = 0;
+
+            await Task.Run(() => taskActionFactory.RandomNumbersTask(++taskNumber, RandomAmount), cts.Token)
+                .ContinueWith(t1 => taskActionFactory.MultiplicationTask(t1.Result, ++taskNumber), cts.Token)
+                .ContinueWith(t2 => taskActionFactory.SortingTask(t2.Result, ++taskNumber), cts.Token)
+                .ContinueWith(t3 => taskActionFactory.GetAverageTask(t3.Result, ++taskNumber), cts.Token);
 
             Console.ReadLine();
         }
---
+++ MultiThreading.Task2.Chaining/TaskActionFactory.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+
+namespace MultiThreading.Task2.Chaining
+{
+    public class TaskActionFactory
+    {
+        private readonly IOutputService _outputService;
+
+        public TaskActionFactory(IOutputService outputService)
+        {
+            _outputService = outputService;
+        }
+
+        public int[] RandomNumbersTask(int taskNumber, int amount)
+        {
+            var result = Actions.GetRandomNumbers(amount);
+
+            _outputService.OutputTaskResult(taskNumber, $"Creates an array of {amount} random integer", Actions.GetArrayString(result));
+
+            return result;
+        }
+
+        public int[] MultiplicationTask(int[] inputArr, int taskNumber)
+        {
+            var result = Actions.MultiplyWithRandom(inputArr, out int multiplicator);
+
+            var outputString = string.Concat($"Multiplicator = {multiplicator}\n", Actions.GetArrayString(inputArr));
+
+            _outputService.OutputTaskResult(taskNumber, "Multiplies this array with another random integer", outputString);
+
+            return result;
+        }
+
+        public int[] SortingTask(int[] inputArr, int taskNumber)
+        {
+            var result = inputArr.OrderBy(x => x).ToArray();
+
+            _outputService.OutputTaskResult(taskNumber, "Sorts this array by ascending", Actions.GetArrayString(result));
+
+            return result;
+        }
+
+        public int GetAverageTask(int[] inputArr, int taskNumber)
+        {
+            var result = (int)inputArr.Average();
+
+            _outputService.OutputTaskResult(taskNumber, "Calculates the average value", $"Average: {result}");
+
+            return result;
+        }
+    }
+}
