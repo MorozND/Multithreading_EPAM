187d080375b44b9b00e88b4d00e5547630d76ce6
Nikita Moroz <nikitamoroz118@gmail.com>
2022-06-07 13:09:33 +0200

    Implement Task 6
    * Configure continuation options
    
    * Implement Task creation
    
    * Refactor
    
    * Update waiting logic
    
    Co-authored-by: Mikita Maroz <Mikita_Maroz@epam.com>

--- MultiThreading.Task6.Continuation/Program.cs
+++ MultiThreading.Task6.Continuation/Program.cs
@@ -7,11 +7,17 @@
    Demonstrate the work of the each case with console utility.
 */
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task6.Continuation
 {
     class Program
     {
+        static readonly CancellationTokenSource _cts = new CancellationTokenSource();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Create a Task and attach continuations to it according to the following criteria:");
@@ -23,8 +29,60 @@ namespace MultiThreading.Task6.Continuation
             Console.WriteLine();
 
             // feel free to add your code
+            var taskRunOption = GetTaskRunOption(args);
+            var taskFactory = new CustomTaskFactory();
+
+            var task = taskFactory.CreateTaskByOption(taskRunOption, _cts.Token);
+
+            var continuations = GetTaskContinuations(task);
+            ProcessTask(task, taskRunOption);
+
+            Task.WaitAny(continuations);
 
             Console.ReadLine();
+        }
+
+        static TaskRunOptions GetTaskRunOption(string[] args)
+        {
+            var optionArgument = args.FirstOrDefault();
+
+            if (optionArgument is null)
+                throw new ArgumentException("No TaskRunOption argument provided");
+
+            if (!int.TryParse(optionArgument, out var intValue))
+                throw new ArgumentException("Can't parse TaskRunOption user input");
+
+            if (!Enum.IsDefined(typeof(TaskRunOptions), intValue))
+                throw new ArgumentException("Invalid TaskRunOption provided");
+
+            return (TaskRunOptions)intValue;
+        }
+
+        static Task[] GetTaskContinuations(Task task)
+        {
+            var result = new List<Task>();
+
+            result.AddRange(
+                TaskContinuationConfigurator.ConfigureAnyContinuation(task)
+            );
+            result.AddRange(
+                TaskContinuationConfigurator.ConfigureFaultedContinuation(task)
+            );
+            result.AddRange(
+                TaskContinuationConfigurator.ConfigureCancelledContinuation(task)
+            );
+
+            return result.ToArray();
+        }
+
+        static void ProcessTask(Task task, TaskRunOptions taskRunOption)
+        {
+            task.Start();
+
+            if (taskRunOption == TaskRunOptions.Cancelled)
+            {
+                _cts.Cancel();
+            }
         }
     }
 }
---
+++ MultiThreading.Task6.Continuation/TaskContinuationConfigurator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MultiThreading.Task6.Continuation
+{
+    public static class TaskContinuationConfigurator
+    {
+        public static IEnumerable<Task> ConfigureAnyContinuation(Task task)
+        {
+            yield return task.ContinueWith(
+                t => Console.WriteLine("Continuation for any result"),
+                TaskContinuationOptions.None
+            );
+        }
+
+        public static IEnumerable<Task> ConfigureFaultedContinuation(Task task)
+        {
+            yield return task.ContinueWith(
+                t =>
+                {
+                    Console.WriteLine("Continuation on faulted");
+                    Console.WriteLine($"Exception message: {t.Exception.Message}");
+                },
+                TaskContinuationOptions.OnlyOnFaulted
+            );
+
+            yield return task.ContinueWith(
+                t =>
+                {
+                    Console.WriteLine("Continuation on faulted and on the antecedent's thread");
+                    Console.WriteLine($"Exception message: {t.Exception.Message}");
+                },
+                TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.ExecuteSynchronously
+            );
+        }
+
+        public static IEnumerable<Task> ConfigureCancelledContinuation(Task task)
+        {
+            yield return task.ContinueWith(
+                t => Console.WriteLine("Continuation on canceled and outside of the thread pool"),
+                TaskContinuationOptions.OnlyOnCanceled
+            );
+        }
+    }
+}
